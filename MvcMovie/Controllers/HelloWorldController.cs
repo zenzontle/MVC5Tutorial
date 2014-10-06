a4d4f9fb465c48aeda17ab1796dae3c026bca3ad
zenzontle <zenzontle@gmail.com>
2014-10-06 16:44:26 -0700

    Initial commit. Four steps completed

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// GET: /HelloWorld/Welcome
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = String.Format("Hello {0}", name);
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+	}
+}
